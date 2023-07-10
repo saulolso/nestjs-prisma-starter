b65b219621a17577cf3fb05393b782bbfdc57532
Beatriz Vaz <beatrizpvaz@Beatrizs-MBP-2.Home>
2023-07-10 13:46:33 +0100

    fix/prisma-import

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostService } from './posts.service';
-import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [PrismaModule],
   providers: [PostService],
   controllers: [PostsController],
 })
